235c8980e8b5810e82309e35849439dd644b1011
HGD87 <hugodelboy@gmail.com>
2024-12-04 19:47:57 -0300

    Add files via upload

---
+++ school-management/src/app/features/teachers/teachers-list/teachers-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teachers-list',
+  imports: [],
+  templateUrl: './teachers-list.component.html',
+  styleUrl: './teachers-list.component.css'
+})
+export class TeachersListComponent {
+
+}
